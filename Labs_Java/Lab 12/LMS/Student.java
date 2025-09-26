3bcd7827ef1284fac9689dbc97aebc73259cc4e8
AzhanAdeem <azhannadeemace11@gmail.com>
2025-09-26 20:02:27 +0500

    Constructing Student class.
    Implemented attributes for student name and seat number in the Student class, along with corresponding getters, setters, and a toString method. Added comments to clarify the association with the CourseList class.

--- Labs_Java/Lab 12/LMS/Student.java
+++ Labs_Java/Lab 12/LMS/Student.java
@@ -1,3 +1,45 @@
+
+// This is the student class that uses the courselist class 
+// The Student class is associatied with the courselist class
+
+// Each studnt will create it's own object and have their separate courses.
+
 public class Student {
+
+    //Attributes
+
+    private String stuName;
+    private String seatNum;
+
+    public Student() {}
+
+    // Setters and Getters
+
+    void setName(String name){
+        this.stuName = name;
+    }
+
+    void setSeatNum(String num){
+        this.seatNum = num;
+    }
+
+    String getName(){
+        return this.stuName;
+    }
+
+    String getSeatNum() {
+        return this.seatNum;
+    }
+
+    //@Overriding interprise methods;
+
+    @Override
+    public String toString(){
+
+        return "Student Name: "+ this.getName() + "Seat Num:"+this.getSeatNum()+"\n";
+    }
+
+
+
     
 }
